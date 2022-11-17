47eab1bfd94318fe23dab82d0a8a4ef71f656a8b
fenris <fenristhefox@gmail.com>
2022-11-17 00:08:53 -0600

    need to make an encoder before I finish this

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 //
 // Created by fenris on 11/16/22.
 //
+#include <cstdlib>
 #include <iostream>
 
 int main() {
+    printf("String we are going to encrypt: \"hello\"\n");
+    printf("I'm gonna use a 8 bit key to keep things simple. The key will be \"53a267df\"\n");
+
+    int key = 0x53a267df;
+    std::string string = "hello";
+    std::string result = "=";
+
+    unsigned int stringSize = string.length();
+    for (int i = 0; i < stringSize; i++) {
+
+    }
 
 
-    std::cout << "just git stuff";
+
+
+    printf("Encryption results in: %s", "4");
+
+
+
+    printf("Decryption results in: %s", "4");
 
     return 0;
 }
